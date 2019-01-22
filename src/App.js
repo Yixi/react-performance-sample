5ab765688a3a4b540ffd3997aea91d0af0621883
Yixi <lyh1112@gmail.com>
2019-01-22 11:08:37 +0800

    base structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {HashRouter as Router, Route, Link} from "react-router-dom"
+import Key from "./key";
+import Home from "./main";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div id="App">
+          <header>
+            <ul>
+              <li>
+                <Link to="/">Index</Link>
+              </li>
+              <li>
+                <Link to="/key">Key</Link>
+              </li>
+            </ul>
+          </header>
+          <div id="content">
+            <Route path="/" exact component={Home}/>
+            <Route path="/key" component={Key}/>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/key/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css"
+
+export default class Key extends React.PureComponent {
+  render() {
+    return (
+      <div className="key-page">
+        key
+      </div>
+    );
+  }
+}
