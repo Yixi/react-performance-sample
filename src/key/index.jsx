d2dadaae94c8c8c67c0c1e1754110c52a3e8d19f
Yixi <lyh1112@gmail.com>
2019-01-22 12:44:57 +0800

    key case

--- src/key/index.jsx
+++ src/key/index.jsx
@@ -1,11 +1,63 @@
 import React from "react";
 import "./index.css"
 
+class Item extends React.PureComponent {
+  render() {
+    console.log('render ', this.props.name)
+    return (
+      <div className="item">{this.props.name}</div>
+    );
+  }
+
+  componentDidUpdate() {
+    console.log(`Item ${this.props.name} update`)
+  }
+
+  componentDidMount() {
+    console.log(`Item ${this.props.name} mount`)
+    //request data
+  }
+}
+
 export default class Key extends React.PureComponent {
+
+  state = {
+    list: ['react', 'vue', 'angular']
+  };
+
+  onUnshift = () => {
+    let list = [...this.state.list];
+    list.unshift('emberjs');
+    this.setState({list})
+  };
+
+  onPush = () => {
+    let list = [...this.state.list];
+    list.push('emberjs');
+    this.setState({list})
+  };
+
+  onShift = () => {
+    let list = [...this.state.list];
+    list.shift();
+    this.setState({list});
+  };
+
   render() {
     return (
       <div className="key-page">
-        key
+        <div className="actions">
+          <button onClick={this.onUnshift}>unshift</button>
+          <button onClick={this.onPush}>push</button>
+          <button onClick={this.onShift}>shift</button>
+        </div>
+
+
+        {this.state.list.map((l, index) => (
+          <Item name={l} key={index}/>
+        ))}
+
+
       </div>
     );
   }
